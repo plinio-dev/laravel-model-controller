95d3df1657fe253d58da16a62be122136e21930b
Tommaso P <tommaso.plinio@gmail.com>
2021-05-13 17:43:43 +0200

    For each in file welcome per stampare

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('welcome', ['movies' => $movies]);
+    }
 }
